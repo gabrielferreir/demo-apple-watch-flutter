9abe797af006f7c87107f8e86293617c5530d45a
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2022-05-11 13:10:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Demo with Flutter + Apple Watch
+
+## References
+
+- https://medium.com/@ferrazrx/flutter-apple-watch-swift-b43110dc4545
---
+++ android/app/src/main/kotlin/com/example/demo_watch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_watch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
